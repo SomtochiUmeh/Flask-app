e550d5e99e1953feacbbc29addf81c7cf75ddb9c
Somtochi Umeh <74836691+SomtochiUmeh@users.noreply.github.com>
2023-08-28 00:46:29 -0700

    added in comment section for posts

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -21,3 +21,8 @@ class PostForm(FlaskForm):
 
     content = TextAreaField("Content", validators=[DataRequired()])
     submit = SubmitField("Post")
+
+
+class CommentForm(FlaskForm):
+    content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField("Post")
